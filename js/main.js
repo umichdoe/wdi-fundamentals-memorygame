14c90569754733e46fe3e1a3a624b1397a49b38c
John Ko <johndoe@umich.edu>
2017-05-02 14:20:03 -0700

    finished logic part in js file

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
 
-console.log("User flipped" + " " + cardOne);
-// test
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+cardsInPlay.push(cards[0]);
+console.log("User flipped " + cardsInPlay);
+
+var cardTwo = cards[2];
+cardsInPlay.push(cards[2]);
+console.log("User flipped " + cardsInPlay[1]); 
+
+if (cardsInPlay.length === 2) {
+	//check to see if 2 cards equal
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");	
+	}
+}
