5ae52027e7f40da53b66926734d87fb9a6fa8590
John Ko <johndoe@umich.edu>
2017-05-03 10:16:21 -0700

    added game logic

--- js/main.js
+++ js/main.js
@@ -2,19 +2,28 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cards[0]);
-console.log("User flipped " + cardsInPlay);
-
-var cardTwo = cards[2];
-cardsInPlay.push(cards[2]);
-console.log("User flipped " + cardsInPlay[1]); 
-
-if (cardsInPlay.length === 2) {
-	//check to see if 2 cards equal
+var checkForMatch = function();
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again.");	
+	      console.log("You found a match!");
+	  } else {
+	      console.log("Sorry, try again.");
+	  }
+
+var flipCard = function(cardId) {
+	console.log("User flipped" + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+};
+
+flipCard(0);
+flipCard(2);
+flipCard(checkForMatch);
+
+/*
+	if (cardsInPlay.length === 2) {
+		//check to see if 2 cards equal
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You found a match!");
+		} else {
+			alert("Sorry, try again.");	
+		}
 	}
-}
