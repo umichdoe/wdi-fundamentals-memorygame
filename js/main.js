f5fe19f36a32612ef84cce99246ba3f1baeed9b8
John Ko <johndoe@umich.edu>
2017-05-03 15:19:44 -0700

    checked flipped functions with Emily

--- js/main.js
+++ js/main.js
@@ -2,15 +2,16 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var checkForMatch = function();
+var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 	      console.log("You found a match!");
 	  } else {
 	      console.log("Sorry, try again.");
 	  }
+};
 
 var flipCard = function(cardId) {
-	console.log("User flipped" + cards[cardId]);
+	console.log("User flipped " + cards[cardId]);
 	cardsInPlay.push(cards[cardId]);
 };
 
@@ -27,3 +28,9 @@ flipCard(checkForMatch);
 			alert("Sorry, try again.");	
 		}
 	}
+
+
+/*
+
+
+*/
