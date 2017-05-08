8c205174c16ea4bcfe15a8c587e39b8b65037237
umichdoe <johndoe@umich.edu>
2017-05-07 18:06:56 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	}
+];
 var cardsInPlay = [];
 
 var checkForMatch = function() {
@@ -11,13 +32,24 @@ var checkForMatch = function() {
 };
 
 var flipCard = function(cardId) {
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 };
 
 flipCard(0);
 flipCard(2);
 flipCard(checkForMatch);
+
+var createBoard = function() {
+	for (var i = 0; i <= cardsInPlay.length; i++) {
+	}
+};
+
+var cardElement = document.createElement('img');
+
+cardElement.setAttribute('src',"images/back.png");
 
 /*
 	if (cardsInPlay.length === 2) {
@@ -25,7 +57,7 @@ flipCard(checkForMatch);
 		if (cardsInPlay[0] === cardsInPlay[1]) {
 			alert("You found a match!");
 		} else {
-			alert("Sorry, try again.");	
+			alert("Sorry, try again.");
 		}
 	}
 
